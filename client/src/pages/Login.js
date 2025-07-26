dcd35597448251ec29e8fefac08e317dfe90cb4b
Samik <samik.sphs@gmail.com>
2025-07-27 04:58:28 +0530

    Create Login.js

---
+++ client/src/pages/Login.js
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+function Login() {
+  const [form, setForm] = useState({ email: '', password: '' });
+
+  const handleChange = e => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_API_URL}/auth/login`, form);
+      localStorage.setItem('token', res.data.token);
+      alert('Login successful!');
+    } catch (err) {
+      alert('Login failed');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input name="password" placeholder="Password" type="password" onChange={handleChange} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
+
+export default Login;
