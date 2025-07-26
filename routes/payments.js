a5a83c6ec71283e87351e632a84cdd5809e3a46a
Samik <samik.sphs@gmail.com>
2025-07-27 04:45:19 +0530

    Create payments.js

---
+++ routes/payments.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+
+router.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body;
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount,
+      currency: 'usd',
+    });
+    res.send({ clientSecret: paymentIntent.client_secret });
+  } catch (err) {
+    res.status(500).send({ error: err.message });
+  }
+});
+
+module.exports = router;
